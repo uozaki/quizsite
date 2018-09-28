46130bc85aa345ea848ae052edc62eb3da731a3b
EC2 Default User <ec2-user@ip-172-31-13-143.ap-southeast-1.compute.internal>
2018-09-28 14:46:23 +0000

    update for upload to heroku

--- config/application.rb
+++ config/application.rb
@@ -23,4 +23,5 @@ module Quizsite
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
+  config.assets.initialize_on_precompile = false
 end
