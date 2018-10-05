be5954885a03adadeccc0d6cf3438272dc9d1b41
uozaki <ajj57801@gmail.com>
2018-10-05 11:33:02 +0000

    コミットメッセージ

--- config/application.rb
+++ config/application.rb
@@ -22,8 +22,8 @@ module Quizsite
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.enabled = false
-  
+    
+    config.assets.initialize_on_precompile = false
     
   end
 
